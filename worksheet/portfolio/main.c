2b4c6fe39596c6e3350c1146a342ca7360d8e42e
Jiaqi-stu <cdld0656@leeds.ac.uk>
2026-03-23 08:07:38 +0000

    finish

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -7,8 +7,36 @@
 int main( void ) {
 
     Point p1 = makePoint(0.0,0.0);
+    Point p2 = makePoint(3.0, 4.0);
+    Point p3 = makePoint(1.5, 2.0);
     
     // test your code by calling the functions and printing the output 
+    printf("Point p1: (%f, %f)\n", p1.x, p1.y);
+    printf("Point p2: (%f, %f)\n", p2.x, p2.y);
+
+    printf("samePoint(p1, p1): %d\n", samePoint(p1, p1));
+    printf("samePoint(p1, p2): %d\n", samePoint(p1, p2));
+
+    Line l = makeLine(p1, p2);
+
+    printf("Line length: %f\n", lineLength(l));
+
+    printf("p1 on line: %d\n", pointInLine(p1, l));
+    printf("p3 on line: %d\n", pointInLine(p3, l));
+
+    Point a = makePoint(0, 0);
+    Point b = makePoint(4, 0);
+    Point c = makePoint(0, 3);
+
+    Triangle t = makeTriangle(a, b, c);
+
+    printf("Triangle area: %f\n", triangleArea(t));
+
+    Point inside = makePoint(1, 1);
+    Point outside = makePoint(5, 5);
+
+    printf("Point inside triangle: %d\n", pointInTriangle(inside, t));
+    printf("Point outside triangle: %d\n", pointInTriangle(outside, t));
     
     return 0;
 }
